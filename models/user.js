db9ae20d0c837b777ef000bf438875546f103253
Yatin Utreja <yatinutreja678@gmail.com>
2021-11-24 14:46:18 +0530

    add repo

--- models/user.js
+++ models/user.js
@@ -1,7 +1,8 @@
 const { string } = require('joi');
 const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
 const userSchema = new Schema({
     email: {
         type: String,
